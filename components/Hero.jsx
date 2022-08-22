1f926ae5c3249c6b381f4ea7cb0d6037c78ff976
Paul C <cloudp7@hotmail.com>
2022-08-22 07:06:30 -0700

    app refresh

---
+++ components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Hero() {
+    return (
+        <nav>
+            <h1>Paul Cloud</h1>
+            <h2>Frontend Developer</h2>
+            <small><a>clouddesignskills.com</a></small>
+            <div>
+                <button>Email</button>
+                <button>LinkedIn</button>
+            </div>
+        </nav>
+       
+    )
+    
+}
