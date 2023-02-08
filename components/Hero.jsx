01b095127d0b8eacb1a16c1039aed70d7a6213fd
cloudpc7 <51870878+cloudpc7@users.noreply.github.com>
2023-02-08 14:16:46 -0800

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -5,7 +5,7 @@ export default function Hero() {
         <nav>
             <h3>Paul Cloud</h3>
             <h4>Frontend Developer</h4>
-            <small><a href="clouddesignskills.com" target="_blank">clouddesign</a></small>
+            <small><a href="clouddesignskills.com" target="_blank">CloudDesign</a></small>
             <div className="buttons">
                 <a><button className="btn email"><img src="/Mail.png" className="mail"/>Email</button></a>
                 <a href="https://www.linkedin.com/in/paul-cloud-5358b320a" target="_blank"><button className="btn linkedin"><img src="/linkedin.png" className="link-logo"/>LinkedIn</button></a>
